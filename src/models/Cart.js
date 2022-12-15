575592ab23cc4e11437335ff0a1b96f91c1ff79c
tomasyovino <tomasyovino@gmail.com>
2022-12-15 19:28:39 -0300

    Display Cart functionality

---
+++ src/models/Cart.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const CartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    products: {type: Array, required: true, max: 100},
+    createdAt: {type: Date}
+});
+
+export const CartModel = mongoose.model("Cart", CartSchema);
---
+++ src/routes/cart.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { listUserByIdController } from "../controllers/User.controller.js";
+import { getCartByIdController, addProductToCartController, deleteProductFromCartController } from "../controllers/Carts.controller.js";
+import { createNewOrderController } from "../controllers/Orders.controller.js";
+import { auth } from "../middlewares/auth.js";
+
+const cartRouter = Router();
+
+cartRouter.get("/", auth, async (req, res) => {
+    const userData = await listUserByIdController(req.user._id);
+    const userCart = await getCartByIdController(req.user._id);
+    const productsOnCart = userCart.products;
+    res.render("cart", {
+      userData: userData,
+      data: productsOnCart,
+    });
+});
+
+cartRouter.post("/", async (req, res) => {
+  const data = await req.body;
+  const cart = await addProductToCartController(data.userId, data.productId);
+});
+
+cartRouter.post("/buy", async (req, res) => {
+  const userID = await req.user._id;
+  const userData = await listUserByIdController(userID);
+  const userCart = await getCartByIdController(userID);
+
+  createNewOrderController(userCart.products, userData.email, userData.phoneNumber);
+});
+
+cartRouter.delete("/product", async (req, res) => {
+  const data = req.body;
+  const deleteProductFromCart = await deleteProductFromCartController(data.userId, data.productId);
+  res.send(deleteProductFromCart);
+});
+
+export default cartRouter;
